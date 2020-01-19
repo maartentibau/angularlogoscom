660658c74b2f9d0b91e562119c2e784eba1d789f
Maarten Tibau <maarten@webtrix.be>
2020-01-19 09:42:25 +0100

    chore(website): add Angular Material (#2)

--- apps/angularlogos/src/app/app.module.ts
+++ apps/angularlogos/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' })],
+  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' }), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
