bafce33de187f2bd7c72de9adb4d18d7cd205f76
maartentibau <maarten@webtrix.be>
2020-01-19 10:07:12 +0100

    Auto-generated commit

--- apps/angularlogos/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'angularlogos',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/angularlogos',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
