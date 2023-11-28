c341950df3462025df168a4b6a3e09194f0ca976
Ferdinand Malcher <ferdinand@malcher.media>
2023-11-28 09:54:47 +0100

    feat: add logo/results count

--- apps/angularlogos/src/app/logos-list/logos-list.component.ts
+++ apps/angularlogos/src/app/logos-list/logos-list.component.ts
@@ -1,18 +1,18 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { concat, Subject } from 'rxjs';
-import { debounceTime, first, map, startWith, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { debounceTime, first, map, shareReplay, startWith, switchMap, takeUntil, tap } from 'rxjs/operators';
 
 import { DataService } from '../shared/data.service';
 import { SearchComponent } from '../search/search.component';
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, I18nPluralPipe, NgFor, NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-logos-list',
   standalone: true,
-  imports: [SearchComponent, AsyncPipe, NgIf, NgFor, MatCardModule, MatButtonModule],
+  imports: [SearchComponent, AsyncPipe, NgIf, NgFor, MatCardModule, MatButtonModule, I18nPluralPipe],
   templateUrl: './logos-list.component.html',
   styleUrls: ['./logos-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -29,11 +29,18 @@ export class LogosListComponent {
     takeUntil(this.searchTerm$)
   );
 
-  readonly logos$ = concat(this.firstSearchTerm$, this.searchTerm$).pipe(
-    debounceTime(200),
+  readonly finalSearchTerm$ = concat(this.firstSearchTerm$, this.searchTerm$).pipe(shareReplay(1));
+
+  readonly logos$ = this.finalSearchTerm$.pipe(
+    debounceTime(300),
     tap((term) => this.setSearchQueryParam(term)),
     startWith(''),
-    switchMap((term) => this.dataService.getLogosFiltered(term))
+    switchMap((term) => this.dataService.getLogosFiltered(term)),
+    shareReplay(1)
+  );
+
+  readonly logoCount$ = this.logos$.pipe(
+    map((logos) => logos.length)
   );
 
   randomSeed: string = `?v=${Math.floor(Math.random() * 10)}`;
